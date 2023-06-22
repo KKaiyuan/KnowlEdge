70aa3ae636dd751b6ed084f37fcdefede15cd76e
Loay Al Jahwari <loay.aljahwari@gmail.com>
2023-06-22 16:54:20 -0700

    fix homepage icon router

--- src/pages/Components/Navbar.jsx
+++ src/pages/Components/Navbar.jsx
@@ -26,7 +26,7 @@ const CustomNavbar = styled(Navbar)`
 const NavbarComponent = () => {
   return (
     <CustomNavbar fluid rounded>
-      <Navbar.Brand href="https://flowbite-react.com">
+      <Navbar.Brand href="/">
         <img alt="Logo" className="mr-3 h-6 sm:h-9" src={logo} />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           <img alt="KnowlEdge" className="mr-3 h-6 sm:h-9" src={name} />
