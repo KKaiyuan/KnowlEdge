e56bb394802dfc035f2cc4290bf526f2395a8720
Loay Al Jahwari <loay.aljahwari@gmail.com>
2023-07-06 20:10:59 -0700

    editting .env access

--- server/database/db.js
+++ server/database/db.js
@@ -1,28 +1,26 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config({path: 'ABSOLUTE PATH TO .ENV FILE'});
+const dotenv = require('dotenv').config();
 
 (async () => {
   try {
-    console.log(dotenv.parsed);
-    const url = `mongodb+srv://${process.env.REACT_APP_MONGO_DB_URL}@knowledge.awzzm9g.mongodb.net/?retryWrites=true&w=majority`;
-    console.log(url);
-    //mongodb+srv://test:<password>@knowledge.awzzm9g.mongodb.net/?retryWrites=true&w=majority
+    const mongoDBURL = process.env.REACT_APP_MONGO_DB_URL;
+    const url = `mongodb+srv://${mongoDBURL}@knowledge.awzzm9g.mongodb.net/KnowlEdge?retryWrites=true&w=majority`;
     await mongoose.connect(url, {
       useNewUrlParser: true,
-      useUnifiedTopology: true, 
+      useUnifiedTopology: true,
     });
 
     console.log('Connected to MongoDB');
 
     // const db = mongoose.connection.db;
-   
+
     // const collections = await db.listCollections().toArray();
 
     // console.log('Collections:');
     // collections.forEach((collection) => {
     //   console.log(collection.name);
     // });
-  } catch (error) { 
+  } catch (error) {
     console.error('Error connecting to MongoDB', error);
   }
 })();
