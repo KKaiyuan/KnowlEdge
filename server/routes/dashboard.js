9549ab77db084da771470c4d796dc9139307038f
Loay Al Jahwari <loay.aljahwari@gmail.com>
2023-07-08 20:43:16 -0700

    update votes -- backend

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -115,4 +115,70 @@ router.post('/comments/', async function (req, res, next) {
   }
 });
 
+function findCommentAndUpdateVotes(comments, specificID, newUpvotes) {
+  for (const comment of comments) {
+    if (new ObjectId(comment._id).equals(specificID)) {
+      console.log('found');
+      comment.upvotes = newUpvotes;
+      return comment;
+    }
+    if (comment.replies && comment.replies.length > 0) {
+      const foundComment = findCommentAndUpdateVotes(
+        comment.replies,
+        specificID,
+        newUpvotes
+      );
+      if (foundComment) {
+        return foundComment;
+      }
+    }
+  }
+  return null;
+}
+
+router.put('/comments/:commentId', async function (req, res, next) {
+  const { resourceId, upvotes } = req.body;
+  const { commentId } = req.params;
+
+  // Check if any required variables are null
+  if (!resourceId || !upvotes) {
+    return res.status(400).json({ error: 'Missing required data' });
+  }
+
+  try {
+    const getAllComments = await Comment.findOne({
+      _id: new ObjectId(resourceId),
+    }).lean({ toJSON: { virtuals: true } });
+
+    const allComments = getAllComments.comments;
+    findCommentAndUpdateVotes(allComments, commentId, upvotes);
+
+    updatedComment = await Comment.findOneAndUpdate(
+      { _id: new ObjectId(resourceId) },
+      { $set: { comments: allComments } },
+      { new: true }
+    );
+
+    await Comment.populate(updatedComment, {
+      path: 'comments.sender',
+      model: 'User',
+    });
+
+    await Comment.populate(updatedComment, {
+      path: 'comments.replies.sender',
+      model: 'User',
+    });
+
+    await Comment.populate(updatedComment, {
+      path: 'comments.replies.reply_to',
+      model: 'User',
+    });
+
+    return res.status(200).json(updatedComment);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 module.exports = router;
